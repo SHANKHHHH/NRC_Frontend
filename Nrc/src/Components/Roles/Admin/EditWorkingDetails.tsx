0caf6bf7ab9c3c1be4728aa914ee640922daf62d
KelvinNath <kelvinnath0987@gmail.com>
2025-08-28 13:17:56 +0530

    minor changes

--- Nrc/src/Components/Roles/Admin/EditWorkingDetails.tsx
+++ Nrc/src/Components/Roles/Admin/EditWorkingDetails.tsx
@@ -339,7 +339,7 @@ const EditWorkingDetails: React.FC = () => {
   }
 
   return (
-    <div className="p-6 bg-gray-50 min-h-screen">
+    <div className="p-6  min-h-screen">
       <div className="max-w-7xl mx-auto">
         <h1 className="text-3xl font-bold text-gray-900 mb-8">Edit Working Details</h1>
         
